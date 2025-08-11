bf4e4efcd418a5200fee4011a96170c094d97822
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-11 14:46:57 +0530

    refactor: update UserResource in Filament module

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -42,11 +42,11 @@ class UserResource extends Resource
                     ->required()->columnSpanFull()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->maxLength(255)
-                    ->dehydrateStateUsing(fn($state) => !empty($state) ? bcrypt($state) : null)
-                    ->dehydrated(fn($state) => filled($state)),
+                // Forms\Components\TextInput::make('password')
+                //     ->password()
+                //     ->maxLength(255)
+                //     ->dehydrateStateUsing(fn($state) => !empty($state) ? bcrypt($state) : null)
+                //     ->dehydrated(fn($state) => filled($state)),
                 Forms\Components\Select::make('roles')
                     ->label('Roles')
                     ->multiple()
