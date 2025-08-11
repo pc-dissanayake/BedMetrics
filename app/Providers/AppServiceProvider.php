785209bb6c054bf5b65989383659bbb9fead5168
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-11 15:10:41 +0530

    refactor: update AppServiceProvider logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,28 @@ class AppServiceProvider extends ServiceProvider
 
 
 FilamentColor::register([
+    'slate' => Color::Slate,
+    'gray' => Color::Gray,
+    'zinc' => Color::Zinc,
+    'neutral' => Color::Neutral,
+    'stone' => Color::Stone,
+    'red' => Color::Red,
+    'orange' => Color::Orange,
+    'amber' => Color::Amber,
+    'yellow' => Color::Yellow,
+    'lime' => Color::Lime,
+    'green' => Color::Green,
+    'emerald' => Color::Emerald,
+    'teal' => Color::Teal,
+    'cyan' => Color::Cyan,
+    'sky' => Color::Sky,
+    'blue' => Color::Blue,
     'indigo' => Color::Indigo,
+    'violet' => Color::Violet,
+    'purple' => Color::Purple,
+    'fuchsia' => Color::Fuchsia,
+    'pink' => Color::Pink,
+    'rose' => Color::Rose,
 ]);
     }
 }
