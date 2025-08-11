dd65c0475ad4f2eec442f8969792fe095fe5ecad
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-11 13:37:42 +0530

    feat: update Permission model in pp/Models

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -6,5 +6,10 @@ use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class Permission extends SpatiePermission
 {
+    protected $fillable = [
+        'name',
+        'guard_name',
+        'description',
+    ];
     // You can add custom methods or relationships here if needed
 }
