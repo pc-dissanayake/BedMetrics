a53de6de98314d6a89f4053c2ec497826fb6eec6
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-11 15:10:11 +0530

    refactor: update AppServiceProvider logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+
+FilamentColor::register([
+    'indigo' => Color::Indigo,
+]);
     }
 }
