b0d50a319fda836b1c6028577296b438d45d2c80
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-11 14:29:29 +0530

    refactor: update UserResource in Filament module

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -17,7 +17,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'fas-user-friends';
 
     public static function form(Form $form): Form
     {
