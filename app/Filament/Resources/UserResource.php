26f3cb5d66299a1856367df520011de08cb3d2c1
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2025-08-11 14:48:14 +0530

    refactor: update UserResource in Filament module

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -67,7 +67,8 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
-                    ->searchable(),
+                    ->searchable(),            Tables\Columns\TextColumn::make('email')
+
                 Tables\Columns\TextColumn::make('email_verified_at')
                     ->dateTime()
                     ->sortable()
